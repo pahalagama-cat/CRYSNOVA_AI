e047843c73eff3f7357a1c5ecca7a7e166ddc3f4
CRYSTAL-LEVI UZOUKWU <carayasata1la@gmail.com>
2026-04-10 00:43:46 +0100

    Create Autorecording.js
    Signed-off-by: CRYSTAL-LEVI UZOUKWU <carayasata1la@gmail.com>

---
+++ src/Commands/Owner/Autorecording.js
@@ -0,0 +1,24 @@
+const { getVar, setVar } = require('../../Plugin/configManager');
+
+module.exports = {
+    name: 'autorecording',
+    alias: ['autorec', 'recording'],
+    desc: 'Toggle auto recording status (shows "recording..." while bot processes)',
+    category: 'Owner',
+    usage: '.autorecording on/off',
+
+    execute: async (sock, m, { args, reply }) => {
+        const action = args[0]?.toLowerCase();
+
+        if (action === 'on') {
+            setVar('AUTO_RECORDING', true);
+            return reply('`✓ Auto Recording ENABLED`');
+        } else if (action === 'off') {
+            setVar('AUTO_RECORDING', false);
+            return reply('`✘ Auto Recording DISABLED`');
+        } else {
+            const current = getVar('AUTO_RECORDING', true);
+            return reply(`Auto Recording is currently: *${current ? 'ON' : 'OFF'}*\n\nUsage: .autorecording on/off`);
+        }
+    }
+};
