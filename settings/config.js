cd5881ae6e1909fe8ab5dc3de27866cc4b1297d6
CRYSTAL-LEVI UZOUKWU <carayasata1la@gmail.com>
2026-04-14 15:01:36 +0100

    Update config.js
    Signed-off-by: CRYSTAL-LEVI UZOUKWU <carayasata1la@gmail.com>

--- settings/config.js
+++ settings/config.js
@@ -79,7 +79,7 @@ const config = {
         process.env.THUMB_URL        ||
         getVar('THUMB_URL')          ||
         userConfig?.thumbUrl         ||
-        'https://media.crysnovax.workers.dev/9669c08e-1272-41ac-8957-f8f03b3daf51.png',
+        'https://cdn.crysnovax.link/files/1776175087921-35676c31-34d6-4fb0-97fa-41397533ca10.jpeg',
 
     // ════════════════════════════════════════════
     // BOT STATUS / MODE (ZEE BOT .env style)
