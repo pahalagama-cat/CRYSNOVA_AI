1e0bbf3f92df6aba728253b056dc4b7918c62f9b
CRYSTAL-LEVI UZOUKWU <carayasata1la@gmail.com>
2026-04-19 09:56:21 +0100

    Add reaction config and update message formatting
    Signed-off-by: CRYSTAL-LEVI UZOUKWU <carayasata1la@gmail.com>

--- src/Commands/Group/gpp.js
+++ src/Commands/Group/gpp.js
@@ -5,15 +5,21 @@ module.exports = {
     desc: 'Download group profile picture',
     category: 'Utils',
     groupOnly: true,
+     // ⭐ Reaction config
+    reactions: {
+        start: '☺️',
+        success: '👌'
+    },
+    
     execute: async (sock, m, { reply, groupMeta }) => {
         try {
             const url = await sock.profilePictureUrl(m.chat, 'image');
             const res = await axios.get(url, { responseType: 'arraybuffer' });
             await sock.sendMessage(m.sender, {
                 image: Buffer.from(res.data),
-                caption: `📸 *Group Picture*\n👥 ${groupMeta?.subject || 'Group'}`
+                caption: `📸 ${groupMeta?.subject || 'Group'}`
             });
-            await reply(`✦ _*Group picture sent to your DM!*_`);
+            await reply(`_*✦ Sent to your DM!*_`);
         } catch { await reply('✘ _*This group has no profile picture!*_'); }
     }
 };
